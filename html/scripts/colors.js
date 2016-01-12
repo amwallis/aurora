04fef9f33da6dda5a7b4252d8f552e7a0d892279
Andrew Wallis <amwallis@mtu.edu>
2016-01-12 03:18:39 +0000

    passing xy coord to python

--- html/scripts/colors.js
+++ html/scripts/colors.js
@@ -1,10 +1,13 @@
 
 function colorCoord(event) {
     event = event || window.event;
-    
+    var x;
+    var y;
     var canvas = document.getElementById('colorPicker'),
         x = event.pageX - canvas.offsetLeft,
         y = event.pageY - canvas.offsetTop;
-    
+    var args = "fwd"
+    var pos = [ x, y ];
+    webiopi().callMacro("get_color", pos);
     alert(x + ' ' + y);
 }
--- html/scripts/lightControl.py
+++ html/scripts/lightControl.py
@@ -3,11 +3,16 @@ import datetime
 
 GPIO = webiopi.GPIO
 
+RED_PIN = 26
+GRN_PIN = 19
+BLU_PIN = 13
+
 # setup function is automatically called at WebIOPi startup
 def setup():
     # set the GPIO used by the light to output
-    #GPIO.setFunction(LIGHT, GPIO.OUT)
-
+    GPIO.setFunction(RED_PIN, GPIO.OUT)
+    GPIO.setFunction(GRN_PIN, GPIO.OUT)
+    GPIO.setFunction(BLU_PIN, GPIO.OUT)
 
 # loop function is repeatedly called by WebIOPi 
 def loop():
@@ -17,4 +22,12 @@ def loop():
 
 # destroy function is called at WebIOPi shutdown
 def destroy():
-    GPIO.digitalWrite(LIGHT, GPIO.LOW)
+    GPIO.digitalWrite(RED_PIN, GPIO.LOW)
+    GPIO.digitalWrite(GRN_PIN, GPIO.LOW)
+    GPIO.digitalWrite(BLU_PIN, GPIO.LOW)
+
+@webiopi.macro
+def get_color(x, y):
+    print ("hi")
+    print(x)
+    print(y)
