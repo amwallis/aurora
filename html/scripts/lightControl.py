b6117cca90fbfd64e880b2eb5d59ffb82aba82a8
Andrew Wallis <amwallis@mtu.edu>
2016-02-13 16:50:07 +0000

    added demo form to html and pwm control for more color variety

--- html/scripts/lightControl.py
+++ html/scripts/lightControl.py
@@ -1,27 +1,41 @@
 
 #SETUP PYTHON PATH
-import sys
-sys.path.append('/usr/local/lib/python3.2/dist-packages/Pillow-3.1.0-py3.2-linux-armv71.egg/PIL/')
+# import sys
+# sys.path.append('/usr/local/lib/python3.2/dist-packages/Pillow-3.1.0-py3.2-linux-armv71.egg/PIL/')
 
-import Image
+# import Image
 import webiopi
-import datetime
+import RPi.GPIO as GPIO
+import time
 
-GPIO = webiopi.GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+# GPIO = webiopi.GPIO
 
 RED_PIN = 26
 GRN_PIN = 19
 BLU_PIN = 13
 
-im = Image.open("../img/colorPicker.png")
-rgb_im = im.convert('RGB')
+GPIO.setup(RED_PIN, GPIO.OUT)
+GPIO.setup(GRN_PIN, GPIO.OUT)
+GPIO.setup(BLU_PIN, GPIO.OUT)
+pR = GPIO.PWM(RED_PIN, 70)
+pG = GPIO.PWM(GRN_PIN, 70)
+pB = GPIO.PWM(BLU_PIN, 70)
+
+# im = Image.open("../img/colorPicker.png")
+# rgb_im = im.convert('RGB')
 
 # setup function is automatically called at WebIOPi startup
 def setup():
     # set the GPIO used by the light to output
-    GPIO.setFunction(RED_PIN, GPIO.OUT)
-    GPIO.setFunction(GRN_PIN, GPIO.OUT)
-    GPIO.setFunction(BLU_PIN, GPIO.OUT)
+    # GPIO.setFunction(RED_PIN, GPIO.OUT)
+    # GPIO.setFunction(GRN_PIN, GPIO.OUT)
+    # GPIO.setFunction(BLU_PIN, GPIO.OUT
+    
+    pR.start(0)
+    pG.start(0)
+    pB.start(0)
 
 # loop function is repeatedly called by WebIOPi 
 def loop():
@@ -31,14 +45,24 @@ def loop():
 
 # destroy function is called at WebIOPi shutdown
 def destroy():
+    pG.stop()
+    pR.stop()
+    pB.stop()
     GPIO.digitalWrite(RED_PIN, GPIO.LOW)
     GPIO.digitalWrite(GRN_PIN, GPIO.LOW)
     GPIO.digitalWrite(BLU_PIN, GPIO.LOW)
+
+@webiopi.macro
+def set_color(red, green, blue):
+    print ("ruuning macro")
+    pR.ChangeDutyCycle(float(red))
+    pG.ChangeDutyCycle(float(green))
+    pB.ChangeDutyCycle(float(blue))
 
 @webiopi.macro
 def get_color(x, y):
     print ("hi")
     print(x)
     print(y)
-    r, g, b = rgb_im.getpixel((x, y))
-    print (r, g, b)
+    # r, g, b = rgb_im.getpixel((x, y))
+    # print (r, g, b)
--- html/scripts/main.js
+++ html/scripts/main.js
@@ -1,16 +1,16 @@
 webiopi().ready(function() {
-    webiopi().setFunction(26,"out");
-    webiopi().setFunction(19,"out");
-    webiopi().setFunction(13,"out");
+    // webiopi().setFunction(26,"out");
+    // webiopi().setFunction(19,"out");
+    // webiopi().setFunction(13,"out");
     
-    var content, buttonR, buttonG, buttonB;
-    content = $("#content");
+    // var content, buttonR, buttonG, buttonB;
+    // content = $("#content");
     
-    buttonR = webiopi().createGPIOButton(26,"red");
-    buttonG = webiopi().createGPIOButton(19,"green");
-    buttonB = webiopi().createGPIOButton(13,"blue");
+    // buttonR = webiopi().createGPIOButton(26,"red");
+    // buttonG = webiopi().createGPIOButton(19,"green");
+    // buttonB = webiopi().createGPIOButton(13,"blue");
 
-    content.append(buttonR);
-    content.append(buttonG);
-    content.append(buttonB);                
+    // content.append(buttonR);
+    // content.append(buttonG);
+    // content.append(buttonB);                
 });
